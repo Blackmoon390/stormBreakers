7dfe0e6e13e4999f1a100b8ec3e9da499d1f1a4d
Blackmoon390 <amsama1134@gmail.com>
2025-04-13 01:37:13 +0530

    Add files via upload
    feat: database added

---
+++ database_admin.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+def show_users():
+    conn = sqlite3.connect("consultant.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM doctors")
+    users = cursor.fetchall()
+    conn.close()
+    return users 
+ 
+
+print(show_users())
